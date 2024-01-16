f4fe4d259385403c4d499d24319b68b6c1d8bcd8
Usov_Artem <artemka063@gmail.com>
2024-01-16 14:43:47 +0400

    feat: add date selection logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+  handleDateChange = (e) => {
+    const selectedDate = new Date(e.target.value);
+    const month = selectedDate.getMonth() + 1;
+    const day = selectedDate.getDate();
 
-function App() {
-  const [count, setCount] = useState(0)
+    let selectedSign = this.state.selectedSign;
 
+    if ((month === 3 && day >= 21) || (month === 4 && day <= 19)) {
+      selectedSign = "aries";
+    } else if ((month === 4 && day >= 20) || (month === 5 && day <= 20)) {
+      selectedSign = "taurus";
+    } else if ((month === 5 && day >= 21) || (month === 6 && day <= 20)) {
+      selectedSign = "gemini";
+    } else if ((month === 6 && day >= 21) || (month === 7 && day <= 22)) {
+      selectedSign = "cancer";
+    } else if ((month === 7 && day >= 23) || (month === 8 && day <= 22)) {
+      selectedSign = "leo";
+    } else if ((month === 8 && day >= 23) || (month === 9 && day <= 22)) {
+      selectedSign = "virgo";
+    } else if ((month === 9 && day >= 23) || (month === 10 && day <= 22)) {
+      selectedSign = "libra";
+    } else if ((month === 10 && day >= 23) || (month === 11 && day <= 21)) {
+      selectedSign = "scorpio";
+    } else if ((month === 11 && day >= 22) || (month === 12 && day <= 21)) {
+      selectedSign = "sagittarius";
+    } else if ((month === 12 && day >= 22) || (month === 1 && day <= 19)) {
+      selectedSign = "capricorn";
+    } else if ((month === 1 && day >= 20) || (month === 2 && day <= 18)) {
+      selectedSign = "aquarius";
+    } else if ((month === 2 && day >= 19) || (month === 3 && day <= 20)) {
+      selectedSign = "pisces";
+    }
+
+    this.setState({ selectedDate: e.target.value, selectedSign }, () => {
+      this.handleSignChange({ target: { value: selectedSign } });
+      this.setState({ selectedSign: selectedSign });
+    });
+  };
   return (
     <>
       <div>
